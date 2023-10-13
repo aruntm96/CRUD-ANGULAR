1bd202f6d78c3b68e0c6372ebfe8fdd4e22f2bed
aruntm96 <arunkumar09062002@gmail.com>
2023-10-13 13:53:31 +0530

    add component added

--- FrontEnd/crud-app/src/app/add/add.component.ts
+++ FrontEnd/crud-app/src/app/add/add.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./add.component.css']
 })
 export class AddComponent {
+  name : string = "";
+  id : string = "";
+  language : string = "";
+  framework : string = "";
 
+  constructor(private http: HttpClient)
+  {
+  }
+
+  save()
+  {
+    let bodyData = {
+      "name" : this.name,
+      "id" : this.id,
+      "language" : this.language,
+      "framework" : this.framework 
+  };
+    this.http.post("http://localhost:8000/create",bodyData).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Server saved successfully")
+        this.name = '';
+        this.id = '';
+        this.language  = '';
+        this.framework  = '';
+    });
+  }
 }
